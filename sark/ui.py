d43368e7e1ee41cc1feaa828a327dfd8e10a8fa5
tmr232 <tmr232@github>
2015-01-26 19:03:44 +0200

    Modified the `ui.ActionHandler` class to auto-generate names for the actions (as this is not something you actually want to do).

--- sark/ui.py
+++ sark/ui.py
@@ -1,7 +1,21 @@
+"""
+Reference:
+    http://www.hexblog.com/?p=886
+
+Return values for update:
+    AST_ENABLE_ALWAYS     // enable action and do not call action_handler_t::update() anymore
+    AST_ENABLE_FOR_IDB    // enable action for the current idb. Call action_handler_t::update() when a database is opened/closed
+    AST_ENABLE_FOR_FORM   // enable action for the current form. Call action_handler_t::update() when a form gets/loses focus
+    AST_ENABLE            // enable action - call action_handler_t::update() when anything changes
+
+    AST_DISABLE_ALWAYS    // disable action and do not call action_handler_t::action() anymore
+    AST_DISABLE_FOR_IDB   // analog of ::AST_ENABLE_FOR_IDB
+    AST_DISABLE_FOR_FORM  // analog of ::AST_ENABLE_FOR_FORM
+    AST_DISABLE           // analog of ::AST_ENABLE
+"""
 import idaapi
 
 
-# TODO: use a metaclass to automatically generate names for classes.
 class ActionHandler(idaapi.action_handler_t):
     NAME = None
     TEXT = "Default. Replace me!"
@@ -10,8 +24,15 @@ class ActionHandler(idaapi.action_handler_t):
     ICON = 0
 
     @classmethod
+    def get_name(cls):
+        if cls.NAME is not None:
+            return cls.NAME
+
+        return "{}:{}".format(cls.__name__, id(cls))
+
+    @classmethod
     def register(cls):
-        name = cls.NAME
+        name = cls.get_name()
         text = cls.TEXT
         handler = cls()
         hotkey = cls.HOTKEY
@@ -31,7 +52,7 @@ class ActionHandler(idaapi.action_handler_t):
 
     @classmethod
     def unregister(cls):
-        idaapi.unregister_action(cls.NAME)
+        idaapi.unregister_action(cls.get_name())
 
     def __init__(self):
         idaapi.action_handler_t.__init__(self)
