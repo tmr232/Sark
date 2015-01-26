048c9ae68966a1a12feb759a60ce270a8ff835ec
tmr232 <tmr232@github>
2015-01-26 19:04:29 +0200

    Refactored the function_flow module based on the recent changes to the ui module. This results in much shorter code.

--- plugins/function_flow.py
+++ plugins/function_flow.py
@@ -5,12 +5,6 @@ import sark
 from sark.ui import ActionHandler
 
 
-MARKBLOCK_REACHABLE = "markblock:reachable"
-MARKBLOCK_UNREACHABLE = "markblock:unreachable"
-MARKBLOCK_REACHING = "markblock:reaching"
-MARKBLOCK_NOTREACHING = "markblock:notreaching"
-MARKBLOCK_CLEAR = "markblock:clear"
-
 COLOR_REACHABLE = 0x66EE11
 COLOR_UNREACHABLE = 0x6611EE
 COLOR_REACHING = 0x11EE66
@@ -20,79 +14,41 @@ COLOR_NONE = 0xFFFFFFFF
 
 
 class MarkReachableNodesHandler(ActionHandler):
-    NAME = MARKBLOCK_REACHABLE
     TEXT = "Reachable"
-    def __init__(self):
-        idaapi.action_handler_t.__init__(self)
 
-    def activate(self, ctx):
-        clear_func(idc.here())
-        mark_reachable_nodes(idc.here())
-        return 1
-
-    def update(self, ctx):
-        return idaapi.AST_ENABLE_ALWAYS
+    def _activate(self, ctx):
+        clear_func(ctx.cur_ea)
+        mark_reachable_nodes(ctx.cur_ea)
 
 
 class MarkUnReachableNodesHandler(ActionHandler):
-    NAME = MARKBLOCK_UNREACHABLE
     TEXT = "Unreachable"
 
-    def __init__(self):
-        idaapi.action_handler_t.__init__(self)
-
-    def activate(self, ctx):
-        clear_func(idc.here())
-        mark_unreachable_nodes(idc.here())
-        return 1
-
-    def update(self, ctx):
-        return idaapi.AST_ENABLE_ALWAYS
+    def _activate(self, ctx):
+        clear_func(ctx.cur_ea)
+        mark_unreachable_nodes(ctx.cur_ea)
 
 class MarkReachingNodesHandler(ActionHandler):
-    NAME = MARKBLOCK_REACHING
     TEXT = "Reaching"
 
-    def __init__(self):
-        idaapi.action_handler_t.__init__(self)
+    def _activate(self, ctx):
+        clear_func(ctx.cur_ea)
+        mark_reaching_nodes(ctx.cur_ea)
 
-    def activate(self, ctx):
-        clear_func(idc.here())
-        mark_reaching_nodes(idc.here())
-        return 1
-
-    def update(self, ctx):
-        return idaapi.AST_ENABLE_ALWAYS
 
 class MarkNotReachingNodesHandler(ActionHandler):
-    NAME = MARKBLOCK_NOTREACHING
     TEXT = "Not Reaching"
 
-    def __init__(self):
-        idaapi.action_handler_t.__init__(self)
-
-    def activate(self, ctx):
-        clear_func(idc.here())
-        mark_not_reaching_nodes(idc.here())
-        return 1
-
-    def update(self, ctx):
-        return idaapi.AST_ENABLE_ALWAYS
+    def _activate(self, ctx):
+        clear_func(ctx.cur_ea)
+        mark_not_reaching_nodes(ctx.cur_ea)
 
 
 class MarkClearHandler(ActionHandler):
-    NAME = MARKBLOCK_CLEAR
     TEXT = "Clear"
 
-    def __init__(self):
-        idaapi.action_handler_t.__init__(self)
-
-    def activate(self, ctx):
-        clear_func(idc.here())
-        return 1
-
-    def update(self, ctx):
-        return idaapi.AST_ENABLE_ALWAYS
+    def _activate(self, ctx):
+        clear_func(ctx.cur_ea)
 
 
 class Hooks(idaapi.UI_Hooks):
@@ -104,13 +60,11 @@ class Hooks(idaapi.UI_Hooks):
         # Or here, after the popup is done being populated by its owner.
 
         if idaapi.get_tform_type(form) == idaapi.BWN_DISASM:
-            idaapi.attach_action_to_popup(form, popup, MARKBLOCK_REACHABLE, "Mark/")
-            idaapi.attach_action_to_popup(form, popup, MARKBLOCK_UNREACHABLE, "Mark/")
-            idaapi.attach_action_to_popup(form, popup, MARKBLOCK_REACHING, "Mark/")
-            idaapi.attach_action_to_popup(form, popup, MARKBLOCK_NOTREACHING, "Mark/")
-            idaapi.attach_action_to_popup(form, popup, MARKBLOCK_CLEAR, "Mark/")
-
-
+            idaapi.attach_action_to_popup(form, popup, MarkReachableNodesHandler.get_name(), "Mark/")
+            idaapi.attach_action_to_popup(form, popup, MarkUnReachableNodesHandler.get_name(), "Mark/")
+            idaapi.attach_action_to_popup(form, popup, MarkReachingNodesHandler.get_name(), "Mark/")
+            idaapi.attach_action_to_popup(form, popup, MarkNotReachingNodesHandler.get_name(), "Mark/")
+            idaapi.attach_action_to_popup(form, popup, MarkClearHandler.get_name(), "Mark/")
 
 
 class FunctionFlow(idaapi.plugin_t):
@@ -170,6 +124,7 @@ def mark_reaching_nodes(ea):
         sark.codeblock(descendant).color = COLOR_REACHING
 
     sark.codeblock(ea).color = COLOR_SOURCE
+
 
 def mark_not_reaching_nodes(ea):
     graph = sark.get_nx_graph(ea)
