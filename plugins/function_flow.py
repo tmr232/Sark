d2c25986319eed16da2b44d100a1802017a7423a
tmr232 <tmr232@github>
2015-02-04 09:59:58 +0200

    Added a function-flow module that works with IDA6.6.

--- plugins/function_flow.py
+++ plugins/function_flow.py
@@ -1,16 +1,11 @@
 import idaapi
-import idc
-import networkx as nx
-import sark
+
+from sark.graph import (clear_func,
+                        mark_not_reaching_nodes,
+                        mark_reaching_nodes,
+                        mark_unreachable_nodes,
+                        mark_reachable_nodes)
 from sark.ui import ActionHandler
-
-
-COLOR_REACHABLE = 0x66EE11
-COLOR_UNREACHABLE = 0x6611EE
-COLOR_REACHING = 0x11EE66
-COLOR_NOT_REACHING = 0x1166EE
-COLOR_SOURCE = 0xEE6611
-COLOR_NONE = 0xFFFFFFFF
 
 
 class MarkReachableNodesHandler(ActionHandler):
@@ -97,48 +92,15 @@ def PLUGIN_ENTRY():
     return FunctionFlow()
 
 
-def mark_reachable_nodes(ea):
-    graph = sark.get_nx_graph(ea)
-    block_ea = sark.get_block_start(ea)
-    for descendant in nx.descendants(graph, block_ea):
-        sark.codeblock(descendant).color = COLOR_REACHABLE
-
-    sark.codeblock(ea).color = COLOR_SOURCE
 
 
-def mark_unreachable_nodes(ea):
-    graph = sark.get_nx_graph(ea)
-    block_ea = sark.get_block_start(ea)
-    descendants = nx.descendants(graph, block_ea)
-    for block in sark.flowchart(ea):
-        if block.startEA not in descendants:
-            block.color = COLOR_UNREACHABLE
-
-    sark.codeblock(ea).color = COLOR_SOURCE
 
 
-def mark_reaching_nodes(ea):
-    graph = sark.get_nx_graph(ea)
-    graph = graph.reverse()
-    block_ea = sark.get_block_start(ea)
-    for descendant in nx.descendants(graph, block_ea):
-        sark.codeblock(descendant).color = COLOR_REACHING
-
-    sark.codeblock(ea).color = COLOR_SOURCE
 
 
-def mark_not_reaching_nodes(ea):
-    graph = sark.get_nx_graph(ea)
-    graph = graph.reverse()
-    block_ea = sark.get_block_start(ea)
-    reaching = nx.descendants(graph, block_ea)
-    for node_ea in graph.nodes_iter():
-        if node_ea not in reaching:
-            sark.codeblock(node_ea).color = COLOR_NOT_REACHING
-
-    sark.codeblock(ea).color = COLOR_SOURCE
 
 
-def clear_func(ea):
-    for block in sark.flowchart(ea):
-        block.color = COLOR_NONE
+
+
+
+
---
+++ plugins/function_flow_66.py
@@ -0,0 +1,77 @@
+import idaapi
+
+from sark.graph import (clear_func,
+                        mark_not_reaching_nodes,
+                        mark_reaching_nodes,
+                        mark_unreachable_nodes,
+                        mark_reachable_nodes)
+
+
+def mark_reachable():
+    ea = idaapi.get_screen_ea()
+    clear_func(ea)
+    mark_reachable_nodes(ea)
+
+
+def mark_unreachable():
+    ea = idaapi.get_screen_ea()
+    clear_func(ea)
+    mark_unreachable_nodes(ea)
+
+
+def mark_reaching():
+    ea = idaapi.get_screen_ea()
+    clear_func(ea)
+    mark_reaching_nodes(ea)
+
+
+def mark_not_reaching():
+    ea = idaapi.get_screen_ea()
+    clear_func(ea)
+    mark_not_reaching_nodes(ea)
+
+
+def mark_clear():
+    ea = idaapi.get_screen_ea()
+    clear_func(ea)
+
+
+class FunctionFlow(idaapi.plugin_t):
+    flags = idaapi.PLUGIN_PROC
+    comment = "Show Flow in Functions"
+    help = "Show code flow inside functions"
+    wanted_name = "Function Flow"
+    wanted_hotkey = ""
+
+    def init(self):
+        idaapi.add_menu_item("View/Mark/", "Reachable", None, 0, mark_reachable, tuple())
+        idaapi.add_menu_item("View/Mark/", "Un-Reachable", None, 0, mark_unreachable, tuple())
+        idaapi.add_menu_item("View/Mark/", "Reaching", None, 0, mark_reaching, tuple())
+        idaapi.add_menu_item("View/Mark/", "Not Reaching", None, 0, mark_not_reaching, tuple())
+        idaapi.add_menu_item("View/Mark/", "Clear", None, 0, mark_clear, tuple())
+
+        return idaapi.PLUGIN_KEEP
+
+    def term(self):
+        pass
+
+    def run(self, arg):
+        pass
+
+
+def PLUGIN_ENTRY():
+    return FunctionFlow()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
