788c8ab14336b4d29b2d03e15702a83cb54ebe9f
tmr232 <tmr232@github>
2015-02-23 10:54:55 +0200

    Updated the function strings plugin to new API.

--- plugins/function_strings.py
+++ plugins/function_strings.py
@@ -22,15 +22,24 @@ class FunctionStrings(idaapi.plugin_t):
         idaapi.msg("String References in {}:0x{:08X}\n".format(function.name, function.startEA))
         idaapi.msg("From          To            String\n")
 
-        for line in function.lines:
-            for ea in line.drefs_from:
-                if idaapi.isCode(idaapi.getFlags(ea)):
-                    continue
-                string = sark.read_ascii_string(ea, max_length=100)
-                if not sark.core.is_string_printable(string):
-                    continue
+        for xref in function.xrefs_from:
+            if xref.type.is_code:
+                continue
 
-                idaapi.msg("0x{:08X}    0x{:08X}    {}\n".format(line.ea, ea, repr(string)))
+            string_type = idc.GetStringType(xref.to)
+
+            if string_type is None:
+                continue
+
+            string = idc.GetString(xref.to, strtype=string_type)
+
+            if not string:
+                continue
+
+            # Trim the string for easier display
+            string = string[:100]
+
+            idaapi.msg("0x{:08X}    0x{:08X}    {}\n".format(xref.frm, xref.to, repr(string)))
 
 
 def PLUGIN_ENTRY():
