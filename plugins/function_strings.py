4c526cabb4e9e884e6c310fe5c434c3c21d13bd8
tmr232 <tmr232@github>
2015-02-25 12:07:45 +0200

    Improved the formatting of the function-strings plugin output.

--- plugins/function_strings.py
+++ plugins/function_strings.py
@@ -19,7 +19,7 @@ class FunctionStrings(idaapi.plugin_t):
     def run(self, arg):
         function = sark.Function(idc.here())
 
-        idaapi.msg("String References in {}:0x{:08X}\n".format(function.name, function.startEA))
+        idaapi.msg("\n\nString References in {}:0x{:08X}\n".format(function.name, function.startEA))
         idaapi.msg("From          To            String\n")
 
         for xref in function.xrefs_from:
