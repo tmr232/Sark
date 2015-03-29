60ef89fb031865dfacefb98ab687b39045516351
tmr232 <tmr232@github>
2015-03-29 15:30:08 +0300

    Added memory writing and code to find patches.

--- sark/data.py
+++ sark/data.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import idc
 import idaapi
 
@@ -66,6 +67,31 @@ def dword_to_bytes(dword):
 def read_memory(start, end):
     size = end - start
     return idaapi.get_many_bytes(start, size)
+
+
+def write_memory(start, data, destructive=False):
+    if destructive:
+        idaapi.put_many_bytes(start, data)
+
+    else:
+        idaapi.patch_many_bytes(start, data)
+
+
+PatchedByte = namedtuple("PatchedByte", "ea fpos original patched")
+
+
+def get_patched_bytes(start=None, end=None):
+    start, end = fix_addresses(start, end)
+
+    patched_bytes = dict()
+
+    def collector(ea, fpos, original, patched):
+        patched_bytes[ea] = PatchedByte(ea, fpos, original, patched)
+        return 0
+
+    idaapi.visit_patched_bytes(start, end, collector)
+
+    return patched_bytes
 
 
 def undefine(start, end):
