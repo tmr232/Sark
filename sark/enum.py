9ec117df9ded1c72eeb6b0fa9d947fc625a74a54
tmr232 <tmr232@github>
2015-05-05 18:05:42 +0300

    Added code to enumerate enum members!

--- sark/enum.py
+++ sark/enum.py
@@ -58,3 +58,90 @@ def add_enum_member(enum, name, value):
     if error:
         raise enum_member_error(error, enum, name, value)
 
+
+class Enum(object):
+    def __init__(self, name=None, enum_id=None):
+        # TODO: Make sure only one (name or enum_id) is provided!
+        self._eid = enum_id or get_enum(name)
+
+        self._name
+        self._width
+        self._place
+        self._format
+        self._signed
+        self._bitfield
+
+    @property
+    def name(self):
+        return idaapi.get_enum_name(self._eid)
+
+    @property
+    def width(self):
+        return idaapi.get_enum_width(self._eid)
+
+    @property
+    def comment(self):
+        return idaapi.get_enum_cmt()
+
+    @property
+    def basetype(self):
+        return idaapi.get_enum_base_type()
+
+    def __len__(self):
+        return idaapi.get_enum_size()
+
+    def __getitem__(self, index):
+        if index >= len(self):
+            raise IndexError()
+
+        return EnumMember(enum_id=self._eid, member_id=idaapi.get_enum_member())
+
+
+class EnumMember(object):
+    def __init__(self):
+        self._name
+        self._value
+        self._mask
+        self._mask_name
+        self._enum
+
+
+def iter_bitmasks(eid):
+    bitmask = idaapi.get_first_bmask(eid)
+
+    yield bitmask
+
+    while bitmask != 0xFFFFFFFF:
+        bitmask = idaapi.get_next_bmask(eid, bitmask)
+        yield bitmask
+
+
+def iter_enum_member_values(eid, bitmask):
+    value = idaapi.get_first_enum_member(eid, bitmask)
+
+    yield value
+    while value != 0xFFFFFFFF:
+        value = idaapi.get_next_enum_member(eid, value, bitmask)
+        yield value
+
+
+def iter_serial_enum_member(eid, value, bitmask):
+    cid, serial = idaapi.get_first_serial_enum_member(eid, value, bitmask)
+    while cid != idaapi.BADNODE:
+        yield cid, serial
+        cid, serial = idaapi.get_next_serial_enum_member(cid, serial)
+
+
+def iter_enum_constant_ids(eid):
+    for bitmask in iter_bitmasks(eid):
+        for value in iter_enum_member_values(eid, bitmask):
+            for cid, serial in iter_serial_enum_member(eid, value, bitmask):
+                yield cid
+
+
+def print_enum(name):
+    # First, get the enum_id
+    eid = idaapi.get_enum(name)
+
+    for cid in iter_enum_constant_ids(eid):
+        print idaapi.get_enum_member_name(cid)
