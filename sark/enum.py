ca69f4dc6f51665e2479bb369404507dc646e705
tmr232 <tmr232@github>
2015-05-06 11:41:37 +0300

    Now with full enum interface. Creation still a bit fuzzy, though.

--- sark/enum.py
+++ sark/enum.py
@@ -1,6 +1,8 @@
 import idaapi
-import exceptions
+from . import exceptions
 from awesome.context import ignored
+
+DEFMASK = 0xFFFFFFFF
 
 ENUM_ERROR_MAP = {
     idaapi.ENUM_MEMBER_ERROR_NAME:
@@ -16,14 +18,15 @@ ENUM_ERROR_MAP = {
 }
 
 
-def enum_member_error(err, eid, name, value):
+def enum_member_error(err, eid, name, value, bitmask):
     exception, msg = ENUM_ERROR_MAP[err]
     enum_name = idaapi.get_enum_name(eid)
-    return exception(('add_enum_member(enum="{}", member="{}", value={}) '
+    return exception(('add_enum_member(enum="{}", member="{}", value={}, bitmask=0x{:08X}) '
                       'failed: {}').format(
         enum_name,
         name,
         value,
+        bitmask,
         msg
     ))
 
@@ -41,69 +44,220 @@ def add_enum(name=None, index=idaapi.BADADDR, flags=idaapi.hexflag(), bitfield=F
             get_enum(name)
             raise exceptions.EnumAlreadyExists()
 
-    enum = idaapi.add_enum(index, name, flags)
+    eid = idaapi.add_enum(index, name, flags)
 
-    if enum == idaapi.BADADDR:
+    if eid == idaapi.BADADDR:
         raise exceptions.EnumCreationFailed('Failed creating enum "{}"'.format(name))
 
     if bitfield:
-        idaapi.set_enum_bf(enum, bitfield)
+        idaapi.set_enum_bf(eid, bitfield)
 
-    return enum
+    return Enum(eid)
 
 
-def add_enum_member(enum, name, value):
-    error = idaapi.add_enum_member(enum, name, value)
+def add_enum_member(enum, name, value, bitmask=DEFMASK):
+    error = idaapi.add_enum_member(enum, name, value, bitmask)
 
     if error:
-        raise enum_member_error(error, enum, name, value)
+        raise enum_member_error(error, enum, name, value, bitmask)
+
+
+class EnumComments(object):
+    def __init__(self, eid):
+        super(EnumComments, self).__init__()
+
+        self._eid = eid
+
+    @property
+    def regular(self):
+        return idaapi.get_enum_cmt(self._eid, False)
+
+    @regular.setter
+    def regular(self, comment):
+        success = idaapi.set_enum_cmt(self._eid, comment, False)
+        if not success:
+            raise exceptions.CantSetEnumComment("Cant set enum comment.")
+
+
+    @property
+    def repeat(self):
+        return idaapi.get_enum_cmt(self._eid, True)
+
+    @repeat.setter
+    def repeat(self, comment):
+        success = idaapi.set_enum_cmt(self._eid, comment, True)
+        if not success:
+            raise exceptions.CantSetEnumComment("Cant set enum comment.")
+
+    def __repr__(self):
+        return ("EnumComments("
+                "name={name!r},"
+                " reqular={regular!r},"
+                " repeat={repeat!r})").format(
+            name=Enum(eid=self._eid).name,
+            regular=self.regular,
+            repeat=self.repeat, )
+
+
+class EnumMembers(object):
+    def __init__(self, eid):
+        super(EnumMembers, self).__init__()
+        self._eid = eid
+
+    def __len__(self):
+        return idaapi.get_enum_size(self._eid)
+
+    def __iter__(self):
+        return (EnumMember(cid) for cid in iter_enum_constant_ids(self._eid))
+
+    def add(self, name, value, bitmask=DEFMASK):
+        add_enum_member(self._eid, name, value, bitmask)
+
+    def __getitem__(self, name):
+        for enum_member in self:
+            if enum_member.name == name:
+                return enum_member
+        raise KeyError("No member named {!r}".format(name))
+
+    def remove(self, name):
+        member = self[name]
+        serial = member.serial
+        value = member.value
+        bmask = member.bmask
+
+        success = idaapi.del_enum_member(self._eid, value, serial, bmask)
+        if not success:
+            raise exceptions.CantDeleteEnumMember("Can't delete enum member {!r}.".format(name))
 
 
 class Enum(object):
-    def __init__(self, name=None, enum_id=None):
-        # TODO: Make sure only one (name or enum_id) is provided!
-        self._eid = enum_id or get_enum(name)
-
-        self._name
-        self._width
-        self._place
-        self._format
-        self._signed
-        self._bitfield
+    def __init__(self, name=None, eid=None):
+        if None not in (name, eid):
+            raise TypeError("Provide only a `name` or an `eid`.")
+        self._eid = eid or get_enum(name)
+        self._comments = EnumComments(self._eid)
 
     @property
     def name(self):
-        return idaapi.get_enum_name(self._eid)
+        return idaapi.get_enum_name(self.eid)
 
     @property
     def width(self):
-        return idaapi.get_enum_width(self._eid)
+        return idaapi.get_enum_width(self.eid)
 
     @property
-    def comment(self):
-        return idaapi.get_enum_cmt()
+    def comments(self):
+        return self._comments
 
     @property
-    def basetype(self):
-        return idaapi.get_enum_base_type()
+    def eid(self):
+        return self._eid
 
-    def __len__(self):
-        return idaapi.get_enum_size()
+    @property
+    def flag(self):
+        return idaapi.get_enum_flag(self.eid)
 
-    def __getitem__(self, index):
-        if index >= len(self):
-            raise IndexError()
+    @property
+    def bitfield(self):
+        return idaapi.is_bf(self.eid)
 
-        return EnumMember(enum_id=self._eid, member_id=idaapi.get_enum_member())
+    @bitfield.setter
+    def bitfield(self, value):
+        success = idaapi.set_enum_bf(self.eid, value)
+        if not success:
+            raise exceptions.CantSetEnumBitfield()
+
+    @property
+    def members(self):
+        return EnumMembers(self.eid)
+
+    def __repr__(self):
+        return "<Enum(name={!r})>".format(self.name)
+
+
+class EnumMemberComments(object):
+    def __init__(self, cid):
+        super(EnumMemberComments, self).__init__()
+
+        self._cid = cid
+
+    @property
+    def regular(self):
+        return idaapi.get_enum_member_cmt(self._cid, False)
+
+    @regular.setter
+    def regular(self, comment):
+        success = idaapi.set_enum_member_cmt(self._cid, comment, False)
+        if not success:
+            raise exceptions.CantSetEnumMemberComment("Cant set enum member comment.")
+
+
+    @property
+    def repeat(self):
+        return idaapi.get_enum_member_cmt(self._cid, True)
+
+    @repeat.setter
+    def repeat(self, comment):
+        success = idaapi.set_enum_member_cmt(self._cid, comment, True)
+        if not success:
+            raise exceptions.CantSetEnumMemberComment("Cant set enum member comment.")
+
+    def __repr__(self):
+        enum_member = EnumMember(self._cid)
+        return ("EnumMemberComments("
+                "name={name!r},"
+                " reqular={regular!r},"
+                " repeat={repeat!r})").format(
+            name="{}.{}".format(enum_member.enum.name, enum_member.name),
+            regular=self.regular,
+            repeat=self.repeat, )
 
 
 class EnumMember(object):
-    def __init__(self):
-        self._name
-        self._value
-        self._mask
-        self._mask_name
-        self._enum
+    def __init__(self, cid):
+        super(EnumMember, self).__init__()
+        self._cid = cid
+        self._comments = EnumMemberComments(self._cid)
+
+    @property
+    def cid(self):
+        return self._cid
+
+    @property
+    def name(self):
+        return idaapi.get_enum_member_name(self.cid)
+
+    @name.setter
+    def name(self, name):
+        success = idaapi.set_enum_member_name(self.cid, name)
+        if not success:
+            raise exceptions.CantRenameEnumMember(
+                "Failed renaming {!r} to {!r}. Does the name exist somewhere else?".format(self.name, name))
+
+    @property
+    def bmask(self):
+        return idaapi.get_enum_member_bmask(self.cid)
+
+    bitmask = bmask
+
+    @property
+    def value(self):
+        return idaapi.get_enum_member_value(self.cid)
+
+    @property
+    def comments(self):
+        return self._comments
+
+    @property
+    def serial(self):
+        return idaapi.get_enum_member_serial(self.cid)
+
+    @property
+    def enum(self):
+        return Enum(eid=idaapi.get_enum_member_enum(self.cid))
+
+    def __repr__(self):
+        return "<EnumMember(name='{}.{}')>".format(self.enum.name, self.name)
 
 
 def iter_bitmasks(eid):
@@ -142,16 +296,3 @@ def iter_enum_constant_ids(eid):
 def iter_enum_ids():
     for index in xrange(idaapi.get_enum_qty()):
         yield idaapi.getn_enum(index)
-
-
-
-def print_enum(name):
-    # First, get the enum_id
-    eid = idaapi.get_enum(name)
-
-    for cid in iter_enum_constant_ids(eid):
-        print idaapi.get_enum_member_name(cid)
-
-def print_enums():
-    for eid in iter_enum_ids():
-        print_enum(idaapi.get_enum_name(eid))
--- sark/exceptions.py
+++ sark/exceptions.py
@@ -86,15 +86,27 @@ class SarkErrorEnumMemberIllv(SarkErrorAddEnumMemeberFailed):
     pass
 
 
-class EnumNotFound(SarkError):
+class SarkEnumError(SarkError):
     pass
 
 
-class EnumCreationFailed(SarkError):
+class EnumNotFound(SarkEnumError):
     pass
 
 
-class EnumAlreadyExists(SarkError):
+class EnumCreationFailed(SarkEnumError):
+    pass
+
+
+class EnumAlreadyExists(SarkEnumError):
+    pass
+
+
+class CantRenameEnumMember(SarkEnumError):
+    pass
+
+
+class CantSetEnumMemberComment(SarkEnumError):
     pass
 
 
@@ -105,17 +117,34 @@ class SarkErrorNameAlreadyExists(SarkError):
 class SarkSetNameFailed(SarkError):
     pass
 
+
 class SarkSwitchError(SarkError):
     pass
+
 
 class SarkNotASwitch(SarkSwitchError):
     pass
 
+
 class SarkNoInstruction(SarkError):
     pass
+
 
 class SarkOperandError(SarkError):
     pass
 
+
 class SarkOperandWithoutReg(SarkOperandError):
+    pass
+
+
+class CantSetEnumComment(SarkEnumError):
+    pass
+
+
+class CantDeleteEnumMember(SarkEnumError):
+    pass
+
+
+class CantSetEnumBitfield(SarkEnumError):
     pass
