e1a63ff8668c1d659da2301cbaafd2b0a0206949
tmr232 <tmr232@github>
2015-05-06 12:02:00 +0300

    Added documentation.

--- sark/enum.py
+++ sark/enum.py
@@ -18,7 +18,8 @@ ENUM_ERROR_MAP = {
 }
 
 
-def enum_member_error(err, eid, name, value, bitmask):
+def _enum_member_error(err, eid, name, value, bitmask):
+    """Format enum member error."""
     exception, msg = ENUM_ERROR_MAP[err]
     enum_name = idaapi.get_enum_name(eid)
     return exception(('add_enum_member(enum="{}", member="{}", value={}, bitmask=0x{:08X}) '
@@ -31,7 +32,8 @@ def enum_member_error(err, eid, name, value, bitmask):
     ))
 
 
-def get_enum(name):
+def _get_enum(name):
+    """Get an existing enum ID"""
     eid = idaapi.get_enum(name)
     if eid == idaapi.BADADDR:
         raise exceptions.EnumNotFound('Enum "{}" does not exist.'.format(name))
@@ -39,9 +41,10 @@ def get_enum(name):
 
 
 def add_enum(name=None, index=idaapi.BADADDR, flags=idaapi.hexflag(), bitfield=False):
+    """Create a new enum."""
     if name is not None:
         with ignored(exceptions.EnumNotFound):
-            get_enum(name)
+            _get_enum(name)
             raise exceptions.EnumAlreadyExists()
 
     eid = idaapi.add_enum(index, name, flags)
@@ -55,14 +58,17 @@ def add_enum(name=None, index=idaapi.BADADDR, flags=idaapi.hexflag(), bitfield=F
     return Enum(eid)
 
 
-def add_enum_member(enum, name, value, bitmask=DEFMASK):
+def _add_enum_member(enum, name, value, bitmask=DEFMASK):
+    """Add an enum member."""
     error = idaapi.add_enum_member(enum, name, value, bitmask)
 
     if error:
-        raise enum_member_error(error, enum, name, value, bitmask)
+        raise _enum_member_error(error, enum, name, value, bitmask)
 
 
 class EnumComments(object):
+    """Enum comments retrieval and manipulation."""
+
     def __init__(self, eid):
         super(EnumComments, self).__init__()
 
@@ -100,26 +106,38 @@ class EnumComments(object):
 
 
 class EnumMembers(object):
+    """Enum members retrieval and manipulation."""
+
     def __init__(self, eid):
         super(EnumMembers, self).__init__()
         self._eid = eid
 
     def __len__(self):
+        """Number of members in the enum"""
         return idaapi.get_enum_size(self._eid)
 
     def __iter__(self):
+        """Iterate all members of the enum"""
         return (EnumMember(cid) for cid in iter_enum_constant_ids(self._eid))
 
     def add(self, name, value, bitmask=DEFMASK):
-        add_enum_member(self._eid, name, value, bitmask)
+        """Add an enum member
+
+        :param name: Name of the member
+        :param value: value of the member
+        :param bitmask: bitmask. Only use if enum is a bitfield.
+        """
+        _add_enum_member(self._eid, name, value, bitmask)
 
     def __getitem__(self, name):
+        """Get an enum member by name."""
         for enum_member in self:
             if enum_member.name == name:
                 return enum_member
         raise KeyError("No member named {!r}".format(name))
 
     def remove(self, name):
+        """Remove an enum member by name"""
         member = self[name]
         serial = member.serial
         value = member.value
@@ -129,36 +147,58 @@ class EnumMembers(object):
         if not success:
             raise exceptions.CantDeleteEnumMember("Can't delete enum member {!r}.".format(name))
 
+    def __repr__(self):
+        return "<EnumMembers(enum={!r}, members=[{}])>".format(
+            Enum(eid=self._eid).name,
+            ", ".join(repr(member.name) for member in self)
+        )
+
 
 class Enum(object):
+    """An enum in the IDB"""
+
     def __init__(self, name=None, eid=None):
+        """
+        Get an existing enum.
+
+        Only provide one of `name` and `eid`.
+        :param name: Name of the enum
+        :param eid: Enum ID
+        """
         if None not in (name, eid):
             raise TypeError("Provide only a `name` or an `eid`.")
-        self._eid = eid or get_enum(name)
+
+        self._eid = eid or _get_enum(name)
         self._comments = EnumComments(self._eid)
 
     @property
     def name(self):
+        """Name of the enum"""
         return idaapi.get_enum_name(self.eid)
 
     @property
     def width(self):
+        """Width of the enum"""
         return idaapi.get_enum_width(self.eid)
 
     @property
     def comments(self):
+        """Enum comments"""
         return self._comments
 
     @property
     def eid(self):
+        """Enum ID"""
         return self._eid
 
     @property
     def flag(self):
+        """Enum flags (bitness, and display type)"""
         return idaapi.get_enum_flag(self.eid)
 
     @property
     def bitfield(self):
+        """Is the enum a bitfield"""
         return idaapi.is_bf(self.eid)
 
     @bitfield.setter
@@ -169,6 +209,7 @@ class Enum(object):
 
     @property
     def members(self):
+        """Get the enum members."""
         return EnumMembers(self.eid)
 
     def __repr__(self):
@@ -176,6 +217,8 @@ class Enum(object):
 
 
 class EnumMemberComments(object):
+    """Enum member comments retrieval and manipulation."""
+
     def __init__(self, cid):
         super(EnumMemberComments, self).__init__()
 
@@ -190,7 +233,6 @@ class EnumMemberComments(object):
         success = idaapi.set_enum_member_cmt(self._cid, comment, False)
         if not success:
             raise exceptions.CantSetEnumMemberComment("Cant set enum member comment.")
-
 
     @property
     def repeat(self):
@@ -214,6 +256,8 @@ class EnumMemberComments(object):
 
 
 class EnumMember(object):
+    """A member of an enum."""
+
     def __init__(self, cid):
         super(EnumMember, self).__init__()
         self._cid = cid
@@ -221,14 +265,21 @@ class EnumMember(object):
 
     @property
     def cid(self):
+        """Get the constant ID"""
         return self._cid
 
     @property
     def name(self):
+        """Get the member name."""
         return idaapi.get_enum_member_name(self.cid)
 
     @name.setter
     def name(self, name):
+        """Set the member name.
+
+        Note that a member name cannot appear in other enums, or generally
+        anywhere else in the IDB.
+        """
         success = idaapi.set_enum_member_name(self.cid, name)
         if not success:
             raise exceptions.CantRenameEnumMember(
@@ -236,24 +287,29 @@ class EnumMember(object):
 
     @property
     def bmask(self):
+        """Get the bitmask"""
         return idaapi.get_enum_member_bmask(self.cid)
 
     bitmask = bmask
 
     @property
     def value(self):
+        """Get the member value"""
         return idaapi.get_enum_member_value(self.cid)
 
     @property
     def comments(self):
+        """Get the member comments"""
         return self._comments
 
     @property
     def serial(self):
+        """Get the member serial (among members of the same value)."""
         return idaapi.get_enum_member_serial(self.cid)
 
     @property
     def enum(self):
+        """Get the enum holding the member."""
         return Enum(eid=idaapi.get_enum_member_enum(self.cid))
 
     def __repr__(self):
@@ -261,6 +317,12 @@ class EnumMember(object):
 
 
 def iter_bitmasks(eid):
+    """Iterate all bitmasks in a given enum.
+
+    Note that while 0xFFFFFFFF indicates no-more-bitmasks, it is also a
+    valid bitmask value. The only way to tell if it exists is when iterating
+    the serials.
+    """
     bitmask = idaapi.get_first_bmask(eid)
 
     yield bitmask
@@ -271,6 +333,11 @@ def iter_bitmasks(eid):
 
 
 def iter_enum_member_values(eid, bitmask):
+    """Iterate member values with given bitmask inside the enum
+
+    Note that 0xFFFFFFFF can either indicate end-of-values or a valid value.
+    Iterate serials to tell apart.
+    """
     value = idaapi.get_first_enum_member(eid, bitmask)
 
     yield value
@@ -280,6 +347,11 @@ def iter_enum_member_values(eid, bitmask):
 
 
 def iter_serial_enum_member(eid, value, bitmask):
+    """Iterate serial and CID of enum members with given value and bitmask.
+
+    Here only valid values are returned, as `idaapi.BADNODE` always indicates
+    an invalid member.
+    """
     cid, serial = idaapi.get_first_serial_enum_member(eid, value, bitmask)
     while cid != idaapi.BADNODE:
         yield cid, serial
@@ -287,6 +359,7 @@ def iter_serial_enum_member(eid, value, bitmask):
 
 
 def iter_enum_constant_ids(eid):
+    """Iterate the constant IDs of all members in the given enum"""
     for bitmask in iter_bitmasks(eid):
         for value in iter_enum_member_values(eid, bitmask):
             for cid, serial in iter_serial_enum_member(eid, value, bitmask):
@@ -294,5 +367,11 @@ def iter_enum_constant_ids(eid):
 
 
 def iter_enum_ids():
+    """Iterate the IDs of all enums in the IDB"""
     for index in xrange(idaapi.get_enum_qty()):
         yield idaapi.getn_enum(index)
+
+
+def enums():
+    """Iterate all enums in the IDB"""
+    return (Enum(eid) for eid in iter_enum_ids())
