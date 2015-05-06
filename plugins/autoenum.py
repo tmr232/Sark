d2eefa56bce76f1300d1ae3fc202449527809a66
tmr232 <tmr232@github>
2015-05-06 15:56:09 +0300

    Added a working auto-enum plugin.

--- plugins/autoenum.py
+++ plugins/autoenum.py
@@ -1,8 +1,42 @@
+from collections import defaultdict
 import idaapi
 import sark
-import sark.enum
 import idc
 import sark.exceptions
+
+
+def apply_enum_by_name(enum, member_name):
+    member_value = enum.members[member_name].value
+    for line in sark.lines(*sark.get_selection()):
+        for operand in line.insn.operands:
+            if operand.type.is_imm:
+                if operand.imm == member_value:
+                    idc.OpEnumEx(line.ea, operand.n, enum.eid, enum.members[member_name].serial)
+
+            elif operand.type.is_displ or operand.type.is_phrase:
+                if operand.addr == member_value:
+                    idc.OpEnumEx(line.ea, operand.n, enum.eid, enum.members[member_name].serial)
+
+
+def get_common_value():
+    values = defaultdict(int)
+    for line in sark.lines(*sark.get_selection()):
+        for operand in line.insn.operands:
+            if operand.type.is_imm:
+                values[operand.imm] += 1
+
+            elif operand.type.is_displ or operand.type.is_phrase:
+                values[operand.addr] += 1
+
+    # Ignore 0 as it is usually not interesting
+    values[0] = 0
+    # Get the most common value
+    common_value = max(values.iteritems(), key=lambda x: x[1])[0]
+    return common_value
+
+
+def const_name(enum, value):
+    return "{}_{:X}".format(enum.name.upper(), value)
 
 
 class AutoEnum(idaapi.plugin_t):
@@ -20,6 +54,8 @@ class AutoEnum(idaapi.plugin_t):
         pass
 
     def run(self, arg):
+        common_value = get_common_value()
+
         enum_name = idc.AskStr(self._last_enum, "Enum Name")
         if enum_name is None:
             return
@@ -29,29 +65,50 @@ class AutoEnum(idaapi.plugin_t):
 
         self._last_enum = enum_name
 
-        const_name = idc.AskStr(None, "Const Name")
-        if not const_name:
-            return
-
-        const_value = idc.AskLong(0, "Const Value")
+        const_value = idc.AskLong(common_value, "Const Value")
         if const_value is None:
             return
 
+        modify = True
+
         try:
-            eid = sark.enum.add_enum(enum_name)
+            enum = sark.add_enum(enum_name)
+
         except sark.exceptions.EnumAlreadyExists:
+            enum = sark.Enum(enum_name)
             yes_no_cancel = idc.AskYN(idaapi.ASKBTN_NO,
                                       "Enum already exists. Modify?\n")
             if yes_no_cancel == idaapi.ASKBTN_CANCEL:
                 return
 
             elif yes_no_cancel == idaapi.ASKBTN_YES:
-                eid = sark.enum.get_enum(enum_name)
+                modify = True
 
             else:  # yes_no_cancel == idaapi.ASKBTN_NO:
-                return
+                modify = False
 
-        sark.enum.add_enum_member(eid, const_name, const_value)
+
+        member_name = const_name(enum, const_value)
+
+        if modify:
+
+            try:
+                enum.members.add(member_name, const_value)
+            except sark.exceptions.SarkErrorAddEnumMemeberFailed as ex:
+                idaapi.msg("[AutoEnum] Adding struct member failed: {}.".format(ex.message))
+
+
+        else:
+            for member in enum.members:
+                if member.value == const_value:
+                    member_name = member.name
+                    break
+                else:
+                    return
+
+
+        # Apply the enum
+        apply_enum_by_name(enum, member_name)
 
 
 def PLUGIN_ENTRY():
