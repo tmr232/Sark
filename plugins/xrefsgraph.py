7d42bb16d779b84f4f4ce87ced2cbb3be35510aa
Tamir Bahar <tamir@north-bit.com>
2016-01-06 18:30:20 +0200

    Better window name for graphs in the xrefsgraph plugin.

--- plugins/xrefsgraph.py
+++ plugins/xrefsgraph.py
@@ -17,6 +17,16 @@ def _try_get_function_start(ea):
     return ea
 
 
+def _get_best_name(ea):
+    try:
+        return sark.Function(ea).demangled
+    except exceptions.SarkNoFunction:
+        name = idc.Name(ea)
+        if name:
+            return name
+        return '0x{:X}'.format(ea)
+
+
 def _xrefs_to(function_ea):
     try:
         return sark.Function(function_ea).xrefs_to
@@ -80,8 +90,11 @@ def show_xref_graph(ea, to=False, distance=4):
 
     call_graph.node[ea][sark.ui.NXGraph.BG_COLOR] = 0x80
 
+    title = 'Xrefs {tofrom} {target} '.format(tofrom='to' if to else 'from',
+                                              target=_get_best_name(ea))
+
     # Create an NXGraph viewer
-    viewer = sark.ui.NXGraph(call_graph, handler=sark.ui.AddressNodeHandler())
+    viewer = sark.ui.NXGraph(call_graph, handler=sark.ui.AddressNodeHandler(), title=title)
 
     # Show the graph
     viewer.Show()
@@ -91,6 +104,7 @@ if idaapi.IDA_SDK_VERSION < 670:  # This means no `idaapi.action_handler_t`. See
     def show_xrefs_from(*args):
         distance = idaapi.asklong(4, 'Distance From Source')
         show_xref_graph(idc.here(), to=False, distance=distance)
+
 
     def show_xrefs_to(*args):
         distance = idaapi.asklong(4, 'Distance To Source')
@@ -108,13 +122,11 @@ if idaapi.IDA_SDK_VERSION < 670:  # This means no `idaapi.action_handler_t`. See
             self.xref_to = idaapi.add_menu_item(MENU_PATH_GRAPHS, 'Xrefs to source', '', 0, show_xrefs_to, None)
             self.xref_from = idaapi.add_menu_item(MENU_PATH_GRAPHS, 'Xrefs from source', '', 0, show_xrefs_from, None)
 
-
             return idaapi.PLUGIN_KEEP
 
         def term(self):
             idaapi.del_menu_item(self.xref_to)
             idaapi.del_menu_item(self.xref_from)
-
 
         def run(self, arg):
             pass
@@ -172,6 +184,7 @@ else:  # IDA 6.7 and higher
 
         def run(self, arg):
             pass
+
 
 def PLUGIN_ENTRY():
     return XrefsGraphPlugins()
