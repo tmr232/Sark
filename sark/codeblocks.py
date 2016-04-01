8af30cc6430c0ca7f302e87e6a9b5ac1fcf1ad50
tmr232 <tmr232@github>
2016-04-01 23:34:56 +0300

    Resetting the color of a node now works as well.

--- sark/codeblocks.py
+++ sark/codeblocks.py
@@ -32,9 +32,13 @@ class CodeBlock(idaapi.BasicBlock):
         for line in self.lines:
             line.color = color
 
-        node_info = idaapi.node_info_t()
-        node_info.bg_color = color
-        idaapi.set_node_info2(self._fc._q.bounds.startEA, self.id, node_info, idaapi.NIF_BG_COLOR)
+        if color is None:
+            idaapi.clr_node_info2(self._fc._q.bounds.startEA, self.id, idaapi.NIF_BG_COLOR)
+
+        else:
+            node_info = idaapi.node_info_t()
+            node_info.bg_color = color
+            idaapi.set_node_info2(self._fc._q.bounds.startEA, self.id, node_info, idaapi.NIF_BG_COLOR)
 
     @property
     def color(self):
