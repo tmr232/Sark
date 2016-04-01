b00353f204a5f95142e9854cfc9a9c8d2881e9d6
Tamir Bahar <tmr232@users.noreply.github.com>
2016-04-01 20:23:20 +0300

    Fix node coloring
    Fix some issues with node coloring, where Sark used the address of the node instead of the address of the function.

--- sark/codeblocks.py
+++ sark/codeblocks.py
@@ -34,11 +34,20 @@ class CodeBlock(idaapi.BasicBlock):
 
         node_info = idaapi.node_info_t()
         node_info.bg_color = color
-        idaapi.set_node_info2(self.startEA, self.id, node_info, idaapi.NIF_BG_COLOR)
+        idaapi.set_node_info2(self._fc._q.bounds.startEA, self.id, node_info, idaapi.NIF_BG_COLOR)
 
     @property
     def color(self):
-        return next(self.lines).color
+        node_info = idaapi.node_info_t()
+        success = idaapi.get_node_info2(node_info, self._fc._q.bounds.startEA, self.id)
+        
+        if not success:
+            return None
+            
+        if not node_info.valid_bg_color():
+            return None
+            
+        return node_info.bg_color
 
     @color.setter
     def color(self, color):
