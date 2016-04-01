8d72a1c0f1b519a574fd94890f0a0b73f73f6a1d
tmr232 <tmr232@github>
2016-04-01 23:41:17 +0300

    Added a missing import.

--- sark/code/line.py
+++ sark/code/line.py
@@ -6,7 +6,7 @@ from ..core import fix_addresses
 from .xref import Xref
 from .instruction import Instruction
 from ..ui import updates_ui
-from .base import get_selection, get_offset_name
+from .base import get_selection, get_offset_name, demangle
 
 
 class Comments(object):
