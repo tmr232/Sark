5c171760056647908e585e225787ef98fae4ce77
Michael Sheinkman <michael.sheinkman@cellebrite.com>
2016-12-18 15:46:17 +0200

    improve formatting

--- sark/code/line.py
+++ sark/code/line.py
@@ -182,16 +182,17 @@ class Line(object):
         return idc.GetDisasm(self.ea)
     @property
     def type(self):
-         properties = {self.is_code:"code",
-                       self.is_data:"data",
-                       self.is_string:"string",
-                       self.is_tail:"tail",
-                       self.is_unknown:"unknown"}
-         for k,v in properties.items():
-             if k: return v
+        """return the type of the Line """
+        properties = {self.is_code:"code",
+                      self.is_data:"data",
+                      self.is_string:"string",
+                      self.is_tail:"tail",
+                      self.is_unknown:"unknown"}
+        for k, v in properties.items():
+            if k: return v
 
     def __repr__(self):
-        return "[{:08X}]    {} {}".format(self.ea, self.disasm,self.type)
+        return "[{:08X}]    {} {}".format(self.ea, self.disasm, self.type)
 
     @property
     def xrefs_from(self):
