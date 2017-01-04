21e0f6dfa95cd7a2c6d30fcd43af00067a4eff15
Michael Sheinkman <michael.sheinkman@cellebrite.com>
2017-01-04 16:01:39 +0200

    updated __init__ file for the exceptions module to import all the exceptions

---
+++ sark/exceptions/__init__.py
@@ -0,0 +1,10 @@
+#__all__ = ["base","general","enum","function","gui","phrase","segment","struct","switch"]
+from .base import *
+from .general import *
+from .enum import *
+from .function import *
+from .gui import *
+from .phrase import *
+from .segment import *
+from .struct import *
+from .switch import *
