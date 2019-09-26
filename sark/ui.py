105c54a16bb9decd2cbf7bfacba2aedf90be2c46
Tamir Bahar <tamir.bahar@gmail.com>
2019-09-26 14:25:04 +0300

    Remove support for pre-6.7 IDA versions

--- sark/ui.py
+++ sark/ui.py
@@ -351,126 +351,124 @@ class NXGraph(idaapi.GraphViewer):
         handler, value, attrs = self._get_handling_triplet(node_id)
         return handler.on_hint(value, attrs)
 
-# Make sure API is supported to enable use of other functionality in older versions.
-# See http://www.hexblog.com/?p=886
-if idaapi.IDA_SDK_VERSION >= 670:
-    class ActionHandler(idaapi.action_handler_t):
-        """A wrapper around `idaapi.action_handler_t`.
 
-        The class simplifies the creation of UI actions in IDA >= 6.7.
+class ActionHandler(idaapi.action_handler_t):
+    """A wrapper around `idaapi.action_handler_t`.
 
-        To create an action, simply create subclass and override the relevant fields
-        and register it::
+    The class simplifies the creation of UI actions in IDA >= 6.7.
 
-            class MyAction(ActionHandler):
-                TEXT = "My Action"
-                HOTKEY = "Alt+Z"
+    To create an action, simply create subclass and override the relevant fields
+    and register it::
 
-                def _activate(self, ctx):
-                    idaapi.msg("Activated!")
+        class MyAction(ActionHandler):
+            TEXT = "My Action"
+            HOTKEY = "Alt+Z"
 
-            MyAction.register()
+            def _activate(self, ctx):
+                idaapi.msg("Activated!")
 
-        Additional Documentation:
-            Introduction to `idaapi.action_handler_t`:
-                http://www.hexblog.com/?p=886
+        MyAction.register()
 
-            Return values for update (from the SDK):
-                AST_ENABLE_ALWAYS     // enable action and do not call action_handler_t::update() anymore
-                AST_ENABLE_FOR_IDB    // enable action for the current idb. Call action_handler_t::update() when a database is opened/closed
-                AST_ENABLE_FOR_FORM   // enable action for the current form. Call action_handler_t::update() when a form gets/loses focus
-                AST_ENABLE            // enable action - call action_handler_t::update() when anything changes
+    Additional Documentation:
+        Introduction to `idaapi.action_handler_t`:
+            http://www.hexblog.com/?p=886
 
-                AST_DISABLE_ALWAYS    // disable action and do not call action_handler_t::action() anymore
-                AST_DISABLE_FOR_IDB   // analog of ::AST_ENABLE_FOR_IDB
-                AST_DISABLE_FOR_FORM  // analog of ::AST_ENABLE_FOR_FORM
-                AST_DISABLE           // analog of ::AST_ENABLE
+        Return values for update (from the SDK):
+            AST_ENABLE_ALWAYS     // enable action and do not call action_handler_t::update() anymore
+            AST_ENABLE_FOR_IDB    // enable action for the current idb. Call action_handler_t::update() when a database is opened/closed
+            AST_ENABLE_FOR_FORM   // enable action for the current form. Call action_handler_t::update() when a form gets/loses focus
+            AST_ENABLE            // enable action - call action_handler_t::update() when anything changes
+
+            AST_DISABLE_ALWAYS    // disable action and do not call action_handler_t::action() anymore
+            AST_DISABLE_FOR_IDB   // analog of ::AST_ENABLE_FOR_IDB
+            AST_DISABLE_FOR_FORM  // analog of ::AST_ENABLE_FOR_FORM
+            AST_DISABLE           // analog of ::AST_ENABLE
+    """
+    NAME = None
+    TEXT = "Default. Replace me!"
+    HOTKEY = ""
+    TOOLTIP = ""
+    ICON = -1
+
+    @classmethod
+    def get_name(cls):
+        """Return the name of the action.
+
+        If a name has not been set (using the `Name` class variable), the
+        function generates a name based on the class name and id.
+        :return: action name
+        :rtype: str
         """
-        NAME = None
-        TEXT = "Default. Replace me!"
-        HOTKEY = ""
-        TOOLTIP = ""
-        ICON = -1
+        if cls.NAME is not None:
+            return cls.NAME
 
-        @classmethod
-        def get_name(cls):
-            """Return the name of the action.
+        return "{}:{}".format(cls.__name__, id(cls))
 
-            If a name has not been set (using the `Name` class variable), the
-            function generates a name based on the class name and id.
-            :return: action name
-            :rtype: str
-            """
-            if cls.NAME is not None:
-                return cls.NAME
+    @classmethod
+    def get_desc(cls):
+        """Get a descriptor for this handler."""
+        name = cls.get_name()
+        text = cls.TEXT
+        handler = cls()
+        hotkey = cls.HOTKEY
+        tooltip = cls.TOOLTIP
+        icon = cls.ICON
+        action_desc = idaapi.action_desc_t(
+            name,
+            text,
+            handler,
+            hotkey,
+            tooltip,
+            icon,
+        )
+        return action_desc
 
-            return "{}:{}".format(cls.__name__, id(cls))
+    @classmethod
+    def register(cls):
+        """Register the action.
 
-        @classmethod
-        def get_desc(cls):
-            """Get a descriptor for this handler."""
-            name = cls.get_name()
-            text = cls.TEXT
-            handler = cls()
-            hotkey = cls.HOTKEY
-            tooltip = cls.TOOLTIP
-            icon = cls.ICON
-            action_desc = idaapi.action_desc_t(
-                name,
-                text,
-                handler,
-                hotkey,
-                tooltip,
-                icon,
-            )
-            return action_desc
+        Each action MUST be registered before it can be used. To remove the action
+        use the `unregister` method.
+        """
+        action_desc = cls.get_desc()
 
-        @classmethod
-        def register(cls):
-            """Register the action.
+        return idaapi.register_action(action_desc)
 
-            Each action MUST be registered before it can be used. To remove the action
-            use the `unregister` method.
-            """
-            action_desc = cls.get_desc()
+    @classmethod
+    def unregister(cls):
+        """Unregister the action.
 
-            return idaapi.register_action(action_desc)
+        After unregistering the class cannot be used.
+        """
+        idaapi.unregister_action(cls.get_name())
 
-        @classmethod
-        def unregister(cls):
-            """Unregister the action.
+    def __init__(self):
+        idaapi.action_handler_t.__init__(self)
 
-            After unregistering the class cannot be used.
-            """
-            idaapi.unregister_action(cls.get_name())
+    def activate(self, ctx):
+        try:
+            self._activate(ctx)
+            return 1
+        except:
+            trace = traceback.format_exc()
+            idaapi.msg("Action {!r} failed to activate. Traceback:\n{}".format(self.get_name(), trace))
+            return 0
 
-        def __init__(self):
-            idaapi.action_handler_t.__init__(self)
+    def update(self, ctx):
+        """Update the action.
 
-        def activate(self, ctx):
-            try:
-                self._activate(ctx)
-                return 1
-            except:
-                trace = traceback.format_exc()
-                idaapi.msg("Action {!r} failed to activate. Traceback:\n{}".format(self.get_name(), trace))
-                return 0
+        Optionally override this function.
+        See IDA-SDK for more information.
+        """
+        return idaapi.AST_ENABLE_ALWAYS
 
-        def update(self, ctx):
-            """Update the action.
+    def _activate(self, ctx):
+        """Activate the action.
 
-            Optionally override this function.
-            See IDA-SDK for more information.
-            """
-            return idaapi.AST_ENABLE_ALWAYS
+        This function contains the action code itself. You MUST implement
+        it in your class for the action to work.
 
-        def _activate(self, ctx):
-            """Activate the action.
-
-            This function contains the action code itself. You MUST implement
-            it in your class for the action to work.
-
-            Args:
-                ctx: The action context passed from IDA.
-            """
-            raise NotImplementedError()
+        Args:
+            ctx: The action context passed from IDA.
+        """
+        raise NotImplementedError()
